62b9e4a20a5c06ede365120bef2f2df28d41f5e1
liana13555 <barishovaliana@gmail.com>
2022-11-21 13:31:48 +0100

    Added service & components

---
+++ photos/src/app/photos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+interface UnsplashResponse {
+  urls: {
+    regular: string
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+  
+export class PhotosService {  
+  constructor(private http: HttpClient) {
+  }
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID 0CICvE736Ao7rdp-O2I7qa1i0FSkelMUPD1v2tMq37Q'
+      }
+    })
+  }
+}
